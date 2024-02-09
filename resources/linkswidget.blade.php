61d9e14eef87eb50ccd6ead755ef0a62f392fd87
SamoSadlaker <samkosadlak@gmail.com>
2024-02-09 19:32:07 +0100

    Open links on new page

--- resources/linkswidget.blade.php
+++ resources/linkswidget.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class="flex flex-wrap p-4">
         @foreach ($links as $link)
-            <a href="{{ $link['url'] }}"
+            <a href="{{ $link['url'] }}" target="_blank"
                 class="mx-1 px-4 py-2 flex flex-col items-center justify-center hover:bg-gray-200 rounded-md group">
                 <div class="text-gray-800"><i class="{{ $link['icon'] }} text-4xl"></i></div>
                 <div class="flex-1 text-center">
